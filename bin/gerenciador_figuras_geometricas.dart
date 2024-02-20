79977bdd363f5b36242bf8a552745c0048c85ecc
aline <linegcruz@gmail.com>
2024-02-20 14:13:37 -0300

    Ajuste da mensagem após instância de uma figura adicionando os pontos x e y.

--- bin/gerenciador_figuras_geometricas.dart
+++ bin/gerenciador_figuras_geometricas.dart
@@ -8,28 +8,28 @@ void main(List<String> arguments) {
 
   Circulo circulo = Circulo(3, 4, 5);
   figuras.add(circulo);
-  print('Criado Circulo');
+  print('Criado Circulo em X=${circulo.x} Y=${circulo.y}');
   print('Área: ${circulo.area()}');
   print('Diâmetro: ${circulo.diametro()}');
   print('');
 
   Retangulo retangulo = Retangulo(1, 2, 4, 4);
   figuras.add(retangulo);
-  print('Criado Retângulo');
+  print('Criado Retângulo em X=${retangulo.x} Y=${retangulo.y}');
   print('Área: ${retangulo.area()}');
   retangulo.isQuadrado();
   print('');
 
-  Retangulo quadrado = Retangulo(9, 5, 7, 3);
-  figuras.add(quadrado);
-  print('Criado Retângulo');
-  print('Área: ${quadrado.area()}');
-  quadrado.isQuadrado();
+  Retangulo retangulo2 = Retangulo(9, 5, 7, 3);
+  figuras.add(retangulo2);
+  print('Criado Retângulo em X=${retangulo2.x} Y=${retangulo2.y}');
+  print('Área: ${retangulo2.area()}');
+  retangulo2.isQuadrado();
   print('');
 
   Triangulo triangulo = Triangulo(5, 6, 3, 3, 3);
   figuras.add(triangulo);
-  print('Criado Triângulo');
+  print('Criado Triângulo em X=${triangulo.x} Y=${triangulo.y} ');
   print('Área: ${triangulo.area()}');
   triangulo.getTipo();
   print('');
