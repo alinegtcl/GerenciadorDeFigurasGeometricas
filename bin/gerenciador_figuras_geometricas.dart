04f21f58fa7817f68ad51dfe76334d45e37f8290
aline <linegcruz@gmail.com>
2024-02-20 13:42:12 -0300

    Adicionada função main iniciando as figuras geométricas, utilizando seus métodos e calculando a área total.

--- bin/gerenciador_figuras_geometricas.dart
+++ bin/gerenciador_figuras_geometricas.dart
@@ -1,4 +1,43 @@
+import 'Circulo.dart';
+import 'Figura.dart';
+import 'Retangulo.dart';
+import 'Triangulo.dart';
 
 void main(List<String> arguments) {
+  List<Figura> figuras = [];
+  double totalArea = 0;
 
+  Circulo circulo = Circulo(3, 4, 5);
+  figuras.add(circulo);
+  print('Criado Circulo');
+  print('Área: ${circulo.area()}');
+  print('Diâmetro: ${circulo.diametro()}');
+  print('');
+
+  Retangulo retangulo = Retangulo(1, 2, 4, 4);
+  figuras.add(retangulo);
+  print('Criado Retângulo');
+  print('Área: ${retangulo.area()}');
+  retangulo.isQuadrado();
+  print('');
+
+  Retangulo quadrado = Retangulo(9, 5, 7, 3);
+  figuras.add(quadrado);
+  print('Criado Retângulo');
+  print('Área: ${quadrado.area()}');
+  quadrado.isQuadrado();
+  print('');
+
+  Triangulo triangulo = Triangulo(5, 6, 3, 3, 3);
+  figuras.add(triangulo);
+  print('Criado Triângulo');
+  print('Área: ${triangulo.area()}');
+  triangulo.getTipo();
+  print('');
+
+  for (var figura in figuras) {
+    totalArea += figura.area();
+  }
+
+  print('Área total dos objetos: $totalArea');
 }
