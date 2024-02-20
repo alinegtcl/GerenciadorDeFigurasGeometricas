048b9938e1a12bab54df023b2aee875327d9146c
aline <linegcruz@gmail.com>
2024-02-20 13:06:00 -0300

    Initial commit

---
+++ test/gerenciador_figuras_geometricas_test.dart
@@ -0,0 +1,8 @@
+import 'package:gerenciador_figuras_geometricas/gerenciador_figuras_geometricas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
